566c241c329375bd8a6d7aac75f3aea0df040449
EC2 Default User <ec2-user@ip-172-31-18-221.us-east-2.compute.internal>
2019-10-23 15:51:25 +0000

    トレーニングメニューの修正

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,16 +1,28 @@
 class Workout < ApplicationRecord
   
-  validates :user_id, presence: true
-  validates :description, presence: true ,length: {maximum: 140}
-  # validates :image, presence: true
-  
+  # アソシエーション
   belongs_to :user
-  # アップローダーを追加
-  mount_uploader :image, ImageUploader
-  
   has_many :likes
   has_many :like_users, through: :likes, source: 'user'
-  
   has_many :comments, dependent: :destroy
   has_many :comment_users, through: :comments, source: 'user'
+  
+  # アップローダー
+  mount_uploader :image, ImageUploader
+  
+  # バリデーション  
+  validates :user_id, presence: true
+  validates :description, presence: true ,length: {maximum: 140}
+  validate :image_size
+  
+  private
+  
+  # アップロードされた画像のサイズをバリデーションする
+  def image_size
+    if image.size > 5.megabytes
+      errors.add(:image, "should be less than 5MB")
+    end
+  end
+    
+
 end
